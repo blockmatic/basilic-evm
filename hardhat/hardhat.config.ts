32510a865407ec2cc042463985549c03392525ce
Gabo Esquivel <contact@gaboesquivel.com>
2024-08-15 10:47:10 -0600

    feat: add hardhat boilerplate

---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,9 @@
+import type { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox-viem'
+import '@nomicfoundation/hardhat-ignition'
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.24',
+}
+
+export default config
