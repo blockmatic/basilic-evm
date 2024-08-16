68ffee57ba6ea6cf8ac73f7d33043c53089e169d
Gabo Esquivel <contact@gaboesquivel.com>
2024-08-15 18:11:25 -0600

    chore: update title

--- apps/webapp/components/layout/header/index.tsx
+++ apps/webapp/components/layout/header/index.tsx
@@ -8,7 +8,7 @@ export function Header() {
     <div className="sticky top-0 z-50 flex h-16 bg-background border-b">
       <div className="container box-content flex flex-row items-center justify-between bg-inherit md:px-4">
         <div className="flex h-full items-center lg:min-w-[100px]">
-          <Link href="/">Basilica Eth</Link>
+          <Link href="/">Basilica EVM</Link>
         </div>
 
         <div className="hidden md:flex md:gap-3 md:pl-4 lg:ml-[-1px] lg:gap-10">
